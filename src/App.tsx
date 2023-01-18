64d45cda87a62eeae27871e8fd617f8a7dae86ce
onezeun <onezeun@gmail.com>
2023-01-18 11:45:03 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">안녕하세용 방갑습니당 ㅋㅋ</div>
+  );
+}
+
+export default App;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  };
+
+  html, body, div, span, h1, h2, h3, h4, h5, h6, p, pre,
+  a, address, img, dl, dt, dd, ol, ul, li, fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td, article, canvas, 
+  footer, header, menu, nav, section, input {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    box-sizing: border-box;
+  };
+
+  html {
+    font-size: 1.4vw;
+    font-family: 'Pretendard-Regular', sans-serif;
+  };
+
+  ol, ul {
+    list-style: none;
+  };
+`;
+
+export default GlobalStyle;
