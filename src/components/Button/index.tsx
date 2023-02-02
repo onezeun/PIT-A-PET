843c1a3ceb6da0cdc4e08116a01332f767c7d970
onezeun <onezeun@gmail.com>
2023-02-02 11:46:36 +0900

    feat: 버튼컴포넌트

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components/macro';
+import { theme } from '../../styles/theme';
+
+interface ButtonStyle {
+  display? : string;
+  width?: string;
+  height?: string;
+  margin?: string;
+  buttonColor?: string;
+  borderRadius?: string;
+  fontColor?: string;
+  fontSize?: string;
+}
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ButtonStyle {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function Button({ className, children, ...rest }: ButtonProps): JSX.Element {
+  return (
+    <ButtonStyled className={className} {...rest}>
+      {children}
+    </ButtonStyled>
+  );
+}
+const ButtonStyled = styled.button<ButtonStyle>`
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  ${({
+    display = 'inline-flex',
+    width = '150px',
+    height = '40px',
+    margin = '0',
+    buttonColor = 'white',
+    borderRadius = '5px',
+    fontColor = 'white',
+    fontSize = '1rem',
+  }) => css`
+    display: ${display};
+    width: ${width};
+    height: ${height};
+    margin: ${margin};
+    background-color: ${(props) => props.theme.colors[buttonColor]};
+    border-radius: ${borderRadius};
+    color: ${(props) => props.theme.colors[fontColor]};
+    font-size: ${fontSize};
+  `}
+`;
+
+export default Button;
