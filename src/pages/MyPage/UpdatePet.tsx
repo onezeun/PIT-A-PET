563454e11c35bdd0041ccb53afeb39c60bad6f98
onezeun <onezeun@gmail.com>
2023-02-06 00:54:50 +0900

    feat: update pet

---
+++ src/pages/MyPage/UpdatePet.tsx
@@ -0,0 +1,155 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store/store';
+import { getPet, updatePet } from 'store/Pet/pet.slice';
+import * as S from './MyPage.styles';
+interface IProps {
+  uid: string;
+  modalClose: React.MouseEventHandler<HTMLButtonElement> | undefined;
+}
+
+export default function UpdatePet({ uid, modalClose }: IProps): JSX.Element {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [petImg, setPetImg] = useState({});
+  const [petImgUrl, setPetImgUrl] = useState('');
+  const [petName, setPetName] = useState('');
+  const [petType, setPetType] = useState('');
+  const [petAge, setPetAge] = useState('모름');
+  const [petGender, setPetGender] = useState('여');
+
+  const [petNameError, setPetNameError] = useState(true);
+  const [petTypeError, setPetTypeError] = useState(true);
+
+  const [petNameErrorMessage, setPetNameErrorMessage] = useState('');
+  const [petTypeErrorMessage, setPetTypeErrorMessage] = useState('');
+
+  const [allCheck, setAllCheck] = useState(false);
+
+  const petImgUpload = useRef<any>();
+
+  const fetchData = () => {
+    dispatch(getPet(uid))
+      .then((data: any) => {
+        console.log(data.payload)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  const petImgChange = (e: any) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPetImgUrl(reader.result as string);
+    }
+    setPetImg(file)
+  }
+
+  const petNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    if (value === '') {
+      setPetNameErrorMessage('반려동물의 이름을 입력해주세요');
+      setPetNameError(true);
+    } else {
+      setPetNameErrorMessage('');
+      setPetNameError(false);
+    }
+    setPetName(value);
+  };
+
+  const petTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    if (value === '') {
+      setPetTypeErrorMessage('반려동물의 종류를 입력해주세요');
+      setPetTypeError(true);
+    } else {
+      setPetTypeErrorMessage('');
+      setPetTypeError(false);
+    }
+    setPetType(value);
+  };
+
+  const addPetCheck = () => {
+    if (petNameError || petTypeError || petImg) {
+      setAllCheck(false);
+      alert('누락된 항목을 확인해주세요')
+    } else setAllCheck(true);
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (allCheck) {
+      dispatch(updatePet({ uid, petImg, petName, petType, petAge, petGender }))
+        .then((data: any) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      return;
+    };
+    modalClose;
+  }
+
+  return (
+    <S.PetBox>
+      <input type="file" className="imgInput" id="imgInput" onChange={petImgChange} ref={petImgUpload}></input>
+      <label htmlFor="imgInput" className='imgLabel'>반려동물 이미지 업로드</label>
+      <S.PetBoxImg
+        src={petImgUrl ? petImgUrl : process.env.PUBLIC_URL + '/images/pet_default_img.png'}
+        id="petImg"
+        alt="프로필 이미지"
+      />
+      <S.PetBoxImgBtn onClick={() => petImgUpload.current.click()}>
+        이미지 업로드
+      </S.PetBoxImgBtn>
+      <S.InputWrap>
+        <S.SubTitle htmlFor="petName" className="subTitle">
+          반려동물 이름
+        </S.SubTitle>
+        <S.PetBoxInput type="text" id="petName" className="addPetInput" onChange={petNameChange}></S.PetBoxInput>
+        <S.PetUpErrMsg>{petNameErrorMessage}</S.PetUpErrMsg>
+      </S.InputWrap>
+      <S.InputWrap>
+        <S.SubTitle htmlFor="petType" className="subTitle">
+          종류
+        </S.SubTitle>
+        <S.PetBoxInput type="text" id="petType" className="addPetInput" placeholder='EX)강아지, 고슴도치, 도마뱀 등' onChange={petTypeChange}></S.PetBoxInput>
+        <S.PetUpErrMsg>{petTypeErrorMessage}</S.PetUpErrMsg>
+      </S.InputWrap>
+      <S.InputWrap>
+        <S.SubTitle htmlFor="petAge" className="subTitle">
+          나이
+        </S.SubTitle>
+        <S.PetBoxSelectInput id="petAge" className="addPetInput" onChange={(e) => setPetAge(e.target.value)}>
+          <option value="none">모름</option>
+          <option value="0~10">0~10</option>
+          <option value="11~20">11~20</option>
+          <option value="21~30">21~30</option>
+          <option value="30이상">30이상</option>
+        </S.PetBoxSelectInput>
+      </S.InputWrap>
+      <S.InputWrap>
+        <S.SubTitle htmlFor="petGender" className="subTitle">
+          성별
+        </S.SubTitle>
+        <S.PetBoxSelectInput id="petGender" className="signUpInput" onChange={(e) => setPetGender(e.target.value)}>
+          <option value="여">여</option>
+          <option value="남">남</option>
+          <option value="기타">기타</option>
+        </S.PetBoxSelectInput>
+      </S.InputWrap>
+      <S.ModalBtnBox>
+        <S.ModalBtn className="cancel" onClick={modalClose} btnColor='GREY'>
+          취소
+        </S.ModalBtn>
+        <S.ModalBtn className="save" onClick={handleSubmit}>
+          저장
+        </S.ModalBtn>
+      </S.ModalBtnBox>
+    </S.PetBox>
+  );
+}
