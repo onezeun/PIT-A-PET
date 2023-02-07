8fc7779192470db20703cd8013e94e9344fc84f4
onezeun <onezeun@gmail.com>
2023-02-07 18:23:56 +0900

    유저 정보 redux에서 불러오기

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 // import * as S from './Main.styles';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 
 import AllContainer from 'components/AllContainer';
 import MessageBox from 'components/MessageBox';
 import Card from 'components/Card';
 
 export default function Main(): JSX.Element {
+  let user = useSelector((state: RootState) => state.auth.sessionData) as any;
+  console.log(user)
   return (
     <AllContainer>
       <MessageBox></MessageBox>
