6d58f41d830137853f660fb8115e0874d341ea9b
onezeun <onezeun@gmail.com>
2023-02-09 17:27:13 +0900

    feat: 채팅방 스크롤 하단 고정

--- src/pages/Chat/ChatRoom.tsx
+++ src/pages/Chat/ChatRoom.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../store/store';
 import { addMessage, getMessage } from 'store/modules/chat.slice';
 
-import AllContainer from 'components/AllContainer';
 import * as S from './Chat.styles';
-
 
 export default function ChatRoom(): JSX.Element {
   const navigate = useNavigate();
@@ -18,14 +16,18 @@ export default function ChatRoom(): JSX.Element {
   let uid = user.uid;
   let date = new Date();
 
-  const [allMessage, setAllMessage] = useState<any[] | null>(null);
-  const [message, setMessage] = useState('');
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [allMessage, setAllMessage] = useState<any | null>(null);
+  const [message, setMessage] = useState<string>('');
   const [render, setRender] = useState('');
 
   useEffect(() => {
     fetchData();
-  }, [render])
-
+    if (scrollRef.current != null) {
+      scrollRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [render]);
+  
   // 메세지 불러오기
   const fetchData = () => {
     dispatch(getMessage(id))
@@ -43,21 +45,22 @@ export default function ChatRoom(): JSX.Element {
     if (message == '') {
       return false;
     } else {
-      dispatch(addMessage({ id, message, date, uid }))
-        .then(() => {
-          setRender(message);
-        })
+      dispatch(addMessage({ id, message, date, uid })).then(() => {
+        setRender(message);
+      });
     }
-  }
+    setMessage('');
+  };
 
-  const handleKeyPress = (e:any) => {
+  const handleKeyPress = (e: any) => {
     if (e.key === 'Enter') {
       sendMessage();
       setMessage('');
     }
   };
+
   return (
-    <AllContainer>
+    <S.ChatRoomContainer>
       <S.ChatWrap>
         <S.ChatRoomOtherInfoWrap>
           <S.BackBtnWrap onClick={() => navigate(-1)}>
@@ -71,15 +74,22 @@ export default function ChatRoom(): JSX.Element {
           </S.OtherInfo>
         </S.ChatRoomOtherInfoWrap>
         <S.MessageBox>
-          {allMessage && allMessage.map((data, i: number) => {
-            return (
-              <S.MagWrap className={data.uid == uid ? 'currentUserWrap' : 'otherWrap'} key={i}>
-                <S.Massage className={data.uid == uid ? 'currentUser' : 'other'}>
-                  <span>{data.message}</span>
-                </S.Massage>
-              </S.MagWrap>
-            )
-          })}
+          {allMessage &&
+            allMessage.map((data: any, i: number) => {
+              return (
+                <S.MagWrap
+                  className={data.uid == uid ? 'currentUserWrap' : 'otherWrap'}
+                  key={i}
+                >
+                  <S.Massage
+                    className={data.uid == uid ? 'currentUser' : 'other'}
+                  >
+                    <span>{data.message}</span>
+                  </S.Massage>
+                </S.MagWrap>
+              );
+            })}
+            <div ref={scrollRef}></div>
         </S.MessageBox>
         <S.MsgInputWrap>
           <S.MsgInput
@@ -87,10 +97,13 @@ export default function ChatRoom(): JSX.Element {
             placeholder="메세지를 입력하세요"
             onChange={messageChange}
             onKeyDown={handleKeyPress}
+            value={message}
           ></S.MsgInput>
-          <S.SendBtn onClick={sendMessage} value={message}>전송</S.SendBtn>
+          <S.SendBtn onClick={sendMessage}>
+            전송
+          </S.SendBtn>
         </S.MsgInputWrap>
       </S.ChatWrap>
-    </AllContainer>
+    </S.ChatRoomContainer>
   );
 }
