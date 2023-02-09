92deeb479bad679709d256a033cf088da7af481e
onezeun <onezeun@gmail.com>
2023-02-09 17:28:24 +0900

    feat: 채팅방 조건 추가 및 스타일 수정

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -4,10 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../store/store';
 import { getChatRoom } from 'store/modules/chat.slice';
 
-import AllContainer from 'components/AllContainer';
 import * as S from './Chat.styles';
 import Button from 'components/Button';
-
 
 export default function Chat(): JSX.Element | null {
   const dispatch = useDispatch<AppDispatch>();
@@ -25,57 +23,76 @@ export default function Chat(): JSX.Element | null {
       chatRoomData();
     }
 
-    if(chatRoomList != null) {
+    if (chatRoomList != null) {
       let chatData: any[] = [];
       chatRoomList.map((list) => {
         let arr = list.userName;
-        let filter = arr.filter((el: string) => el != currentuserName)
+        let filter = arr.filter((el: string) => el != currentuserName);
         let username = filter.shift();
 
-        chatData.push({ id: list.id, name: username })
+        chatData.push({ id: list.id, name: username });
         setChatRoom(chatData);
-      })
+      });
     }
   }, [chatRoomList]);
 
   const chatRoomData = () => {
-    dispatch(getChatRoom(currentUid))
-      .then((data: any) => {
-        let chatRoomData: any[] = [];
-        data.payload.map((data: any) => {
-          chatRoomData.push(data);
-          setChatRoomList(chatRoomData);
-        })
+    dispatch(getChatRoom(currentUid)).then((data: any) => {
+      let chatRoomData: any[] = [];
+      data.payload.map((data: any) => {
+        chatRoomData.push(data);
+        setChatRoomList(chatRoomData);
       });
+    });
   };
-
   return (
-    <AllContainer>
+    <S.ChatRoomContainer>
       <S.ChatWrap>
-        {chatRoom && chatRoom.map((room, i) => {
-          return (
-            <S.ChatListItem onClick={() => navigate(`/chatroom/${room.id}/${room.name}`)} key={i}>
-              <S.OtherInfo>
-                <S.OtherImg
-                  src={process.env.PUBLIC_URL + '/images/profile.png'}
-                ></S.OtherImg>
-                <S.OtherName>{room.name}</S.OtherName>
-                <S.LastMessage>마지막 메세지</S.LastMessage>
-              </S.OtherInfo>
-              <Button
-                width="60px"
-                height="30px"
-                fontSize="0.9rem"
-                buttonColor="BLUE_100"
-                fontColor="WHITE"
+        {chatRoom ? (
+          chatRoom.map((room, i) => {
+            return (
+              <S.ChatListItem
+                onClick={() => navigate(`/chatroom/${room.id}/${room.name}`)}
+                key={i}
               >
-                나가기
+                <S.OtherInfo>
+                  <S.OtherImg
+                    src={process.env.PUBLIC_URL + '/images/profile.png'}
+                  ></S.OtherImg>
+                  <S.OtherName>{room.name}</S.OtherName>
+                  <S.LastMessage>마지막 메세지</S.LastMessage>
+                </S.OtherInfo>
+                <Button
+                  width="60px"
+                  height="30px"
+                  fontSize="0.9rem"
+                  buttonColor="BLUE_100"
+                  fontColor="WHITE"
+                >
+                  나가기
+                </Button>
+              </S.ChatListItem>
+            );
+          })
+        ) : (
+          <>
+            <S.Null>
+              <p>주고받은 메세지가 없습니다.</p>
+              <Button
+                margin="20px auto 0"
+                width="100px"
+                height="40px"
+                fontSize="1rem"
+                buttonColor="YELLOW"
+                fontColor="GREY_200"
+                onClick={() => navigate('/')}
+              >
+                홈으로
               </Button>
-            </S.ChatListItem>
-          )
-        }
+            </S.Null>
+          </>
         )}
       </S.ChatWrap>
-    </AllContainer>
+    </S.ChatRoomContainer>
   );
 }
